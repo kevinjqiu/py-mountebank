dbccecf4fbaa6591d6096789c65a592e410b9168
Kevin Jing Qiu <kevin@idempotent.ca>
2016-04-10 22:17:58 -0400

    More predicates

--- src/mountebank/stub/predicates.py
+++ src/mountebank/stub/predicates.py
@@ -11,17 +11,30 @@ class PredicateBuilder(object):
     def __init__(self, field_name):
         self._field_name = field_name
 
-    def __eq__(self, other):
-        return self.equals(other)
+    def __eq__(self, value):
+        return self.equals(value)
 
-    def equals(self, other):
-        return Predicate('equals', self._field_name, other)
+    def equals(self, value):
+        return Predicate('equals', self._field_name, value)
 
-    def deep_equals(self, other):
-        return Predicate('deepEquals', self._field_name, other)
+    def deep_equals(self, value):
+        return Predicate('deepEquals', self._field_name, value)
 
     def contains(self, other):
-        return Predicate('contains', self._field_name, other)
+        return Predicate('contains', self._field_name, value)
+
+    def starts_with(self, value):
+        return Predicate('startsWith', self._field_name, value))
+
+    def ends_with(self, value):
+        return Predicate('endsWith', self._field_name, value))
+
+    def matches(self, value):
+        return Predicate('matches', self._field_name, value))
+
+    def exists(self, value):
+        assert type(value) == bool, 'exists() must be called with True/False'
+        return Predicate('exists', self._field_name, value)
 
 
 class HTTPRequest(object):
@@ -31,4 +44,3 @@ class HTTPRequest(object):
     method = PredicateBuilder('method')
     headers = PredicateBuilder('headers')
     body = PredicateBuilder('body')
-    inject = PredicateBuilder('inject')
