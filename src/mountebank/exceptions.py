65b1f849cbf02320992e3ef9db86c71e564cc826
Kevin Jing Qiu <kevin@idempotent.ca>
2016-04-15 22:41:51 -0400

    Make Python 2 and 3 compatible

--- src/mountebank/exceptions.py
+++ src/mountebank/exceptions.py
@@ -1,3 +1,14 @@
-class ImposterException(StandardError):
+import sys
+
+
+if sys.version_info.major == 2:
+    error_base_class = StandardError
+elif sys.version_info.major == 3:
+    error_base_class = Exception
+else:
+    raise RuntimeError('Unsupported Python version: {}'.format(sys.version))
+
+
+class ImposterException(error_base_class):
     def __init__(self, response):
         self._response = response
